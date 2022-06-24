7b354046514602315967b407bc21a65d59dabb3e
marrk50 <105142154+marrk50@users.noreply.github.com>
2022-06-24 09:24:10 +0530

    Update sample_config.py

--- FallenRobot/sample_config.py
+++ FallenRobot/sample_config.py
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1356469075  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "anonymous_was_bot"
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your own group for support, do not add the @
+    OWNER_ID = 2030475041  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "marrk85"
+    SUPPORT_CHAT = "marrkmusic"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001686672798
     )  # Prints any new group the bot is added to, prints just the name and ID.
