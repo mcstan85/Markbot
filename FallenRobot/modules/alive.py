51fa32f794478a79d7cdde546c392e8d495b5186
marrk50 <105142154+marrk50@users.noreply.github.com>
2022-06-24 10:52:29 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -12,21 +12,21 @@ from FallenRobot import telethn as tbot
 
 PHOTO = [
     "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
-    "https://telegra.ph/file/40eb1ed850cdea274693e.jpg",
+    "https://te.legra.ph/file/48fc2db877b27ee398200.jpg",
 ]
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ ꜰᴀʟʟᴇɴ ✘ ʀᴏʙᴏᴛ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](https://t.me/anonymous_was_bot)** \n\n"
+    TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ мαяк ✘ ʀᴏʙᴏᴛ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ιɾσɳ ♡](https://t.me/marrk85)** \n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/FallenXRobot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/DevilsHeavenMF"),
+            Button.url("ʜᴇʟᴘ​", "https://t.me/MarrkXRobot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/marrkmusic"),
         ]
     ]
     ran = random.choice(PHOTO)
